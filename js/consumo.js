cf63647f0ae03078a58d5f50bb7e6ae0b968852a
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2016-08-09 13:52:00 -0500

    Ya se cargan los alimentos a la lista, falta checar con respecto a las calorías

--- js/consumo.js
+++ js/consumo.js
@@ -1,32 +1,146 @@
 function getPanelConsumo(){
 	var objMenu = new TMenu;
 	var objCliente = new TCliente;
+	var pComida = "";
+	
+	$.get("vistas/menu/comida.html", function(resultado){
+		pComida = resultado;
+	});
 	
 	$.get("vistas/consumo.html", function(resp){
 		$(".navbar-title").html("Consumo de alimentos");
 		$("#modulo").html(resp);
-		$.get("vistas/menu/comida.html", function(pComida){
-			objMenu.getComidas({
-				before: function(){
-					alertify.log("Estamos obteniendo los datos del servidor..."); 
-				},
+		
+		
+		function getAlimentos(idComida){
+			var plt =  $("[plantillaComida=" + idComida + "]");
+			objMenu.getAlimentos(idComida, {
 				after: function(resp){
-					$.each(resp, function(i, el){
-						var plt = pComida;
-						plt = $(plt);
-						$.each(el, function(key, value){
-							plt.find("[campo=" + key + "]").html(value);
-						})
+					if (resp.length > 0){
+						plt.find("table").find("tbody").find("tr").remove();
+						var cantidad = 0;
 						
-						plt.find(".panel-title").find("a").attr("href", "#comida" + el.idComida);
-						plt.find(".panel-collapse").attr("id", "comida" + el.idComida);
+						$.each(resp, function(key, tr){
+							elemento = $('<tr><td>' + tr.nombre + '</td><td class="text-right">' + (tr.cantidad * 100) + '</td><tr>');
+							plt.find("table").find("tbody").append(elemento);
+							cantidad += parseFloat(tr.cantidad);
+							
+							elemento.click(function(){
+								var el = $(this);
+								alertify.confirm("<p>¿Está seguro de querer eliminar el alimento de la lista?</p>", function (e) {
+									if (e){
+										objMenu.delAlimento(idComida, tr.idAlimento, {
+											before: function(){
+												alertify.log("Se está quitando el alimento de la lista");
+											}, after: function(resp){
+												if (resp.band){
+													el.remove();
+													alertify.success("Se quitó el alimento de la lista");
+												}else
+													alertify.error("No se pudo quitar el alimento de la lista");
+											}
+										});
+									}
+								});
+							});
+						});
 						
-						$("#accordion").append(plt);
-					});
+						cantidad *= 100;
+						plt.find("[campo=totalGramos]").html(cantidad + " g");
+						plt.find(".btnPlantilla").hide();
+					}
 				}
 			});
-			
-			$("#modulo").find("[campo=caloriasDiarias]").html(objCliente.calorias);
+		}
+		
+		objMenu.getComidas({
+			before: function(){
+				alertify.log("Estamos obteniendo los datos del servidor..."); 
+			},
+			after: function(resp){
+				$.each(resp, function(i, el){
+					var plt = pComida;
+					plt = $(plt);
+					$.each(el, function(key, value){
+						plt.find("[campo=" + key + "]").html(value);
+					})
+					
+					plt.find(".panel-title").find("a").attr("href", "#comida" + el.idComida);
+					plt.find(".panel-collapse").attr("id", "comida" + el.idComida);
+					plt.find(".btnAlimentos").attr("comida", el.idComida);
+					plt.find(".btnAlimentos").click(function(){
+						$("#winAlimentos").modal();
+						$("#winAlimentos").find("#comida").val(el.idComida);
+					});
+					
+					plt.attr("plantillaComida", el.idComida);
+					
+					plt.find(".btnPlantilla").click(function(){
+						alertify.confirm("<p>¿Está seguro?</p>", function (e) {
+							if (e){
+								objMenu.setAlimentosPlantilla(el.idComida, {
+									before: function(){
+										alertify.log("Se está cargando la lista, espere por favor...");
+										plt.find(".btnPlantilla").prop("disabled", true);
+									}, after: function(resp){
+										plt.find(".btnPlantilla").prop("disabled", false);
+										
+										if (resp.band){
+											getAlimentos(el.idComida);
+											alertify.success("La plantilla fue cargada con éxito");
+										}else
+											alertify.error("No pudo ser cargada la plantilla");
+									}
+								});
+							}
+						}); 
+					});
+					
+					$("#accordion").append(plt);
+					getAlimentos(el.idComida);
+				});
+			}
 		});
+		
+		$("#modulo").find("[campo=caloriasDiarias]").html(objCliente.calorias);
+		
+		$.post(server + 'listaAlimentos', {
+			"json": true,
+			"movil": '1'
+		}, function(datos){
+			$.get("vistas/menu/listaAlimentos.html", function(pAlimento){
+				$.each(datos, function(key, el){
+					var plt = pAlimento;
+					plt = $(plt);
+					
+					$.each(el, function(key, value){
+						plt.find("[campo=" + key + "]").html(value);
+					});
+					
+					plt.find("#selCantidad").attr("alimento", el.idAlimento);
+					
+					plt.find("#btnAgregar").click(function(){
+						valor = plt.find("#selCantidad").val();
+						
+						objMenu.addAlimento($("#winAlimentos").find("#comida").val(), plt.find("#selCantidad").attr("alimento"), plt.find("#selCantidad").val(), {
+							before: function(){
+								plt.find("#btnAgregar").prop("disabled", true);
+							},
+							after: function(resp){
+								plt.find("#btnAgregar").prop("disabled", false);
+								
+								if(resp.band){
+									getAlimentos($("#winAlimentos").find("#comida").val());
+									alertify.success("Se agregó el alimento a la lista");
+								}else
+									alertify.error("Al parecer este alimento ya está en la lista");
+							}
+						});
+					});
+					
+					$("#winAlimentos").find(".list-group").append(plt);
+				});
+			});
+		}, "json");
 	});
 }
--- js/modelo/menu.class.js
+++ js/modelo/menu.class.js
@@ -1,5 +1,6 @@
 TMenu = function(){
 	var self = this;
+	this.cliente = new TCliente;
 	
 	this.getComidas = function(fn){
 		if (fn.before !== undefined) fn.before();
@@ -7,6 +8,78 @@ TMenu = function(){
 		$.post(server + 'listaComidas', {
 				"movil": '1',
 				"json": true
+			}, function(data){
+				if (data.band == 'false')
+					console.log(data.mensaje);
+					
+				if (fn.after !== undefined)
+					fn.after(data);
+			}, "json");
+	}
+	
+	this.getAlimentos = function(comida, fn){
+		if (fn.before !== undefined) fn.before();
+		
+		$.post(server + 'listaAlimentosMenu', {
+				"movil": '1',
+				"comida": comida,
+				"cliente": self.cliente.idCliente,
+				"json": true
+			}, function(data){
+				if (data.band == 'false')
+					console.log(data.mensaje);
+					
+				if (fn.after !== undefined)
+					fn.after(data);
+			}, "json");
+	}
+	
+	this.setAlimentosPlantilla = function(comida, fn){
+		if (fn.before !== undefined) fn.before();
+		
+		$.post(server + 'cmenus', {
+				"movil": '1',
+				"comida": comida,
+				"action": "setPlantilla",
+				"cliente": self.cliente.idCliente
+			}, function(data){
+				if (data.band == 'false')
+					console.log(data.mensaje);
+					
+				if (fn.after !== undefined)
+					fn.after(data);
+			}, "json");
+
+	}
+	
+	this.addAlimento = function(comida, alimento, cantidad, fn){
+		if (fn.before !== undefined) fn.before();
+		
+		$.post(server + 'cmenus', {
+				"movil": '1',
+				"comida": comida,
+				"alimento": alimento,
+				"cantidad": cantidad,
+				"action": "addAlimento",
+				"cliente": self.cliente.idCliente
+			}, function(data){
+				if (data.band == 'false')
+					console.log(data.mensaje);
+					
+				if (fn.after !== undefined)
+					fn.after(data);
+			}, "json");
+	}
+	
+	this.delAlimento = function(comida, alimento, fn){
+		if (fn.before !== undefined) fn.before();
+		
+		$.post(server + 'cmenus', {
+				"movil": '1',
+				"comida": comida,
+				"alimento": alimento,
+				"action": "delAlimento",
+				"cliente": self.cliente.idCliente
 			}, function(data){
 				if (data.band == 'false')
 					console.log(data.mensaje);
