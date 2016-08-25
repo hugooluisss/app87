4d9667dfa90ba946fab86c0cdcf2b99ae9aa2267
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-08-25 00:18:27 -0500

    Tamaño de pantalla

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 		checkSuscripcion();
 	}else{
 		loadLogin();
-		$("#modulo").css("min-height", ($(document).height() - $(document).height() * 0.05) + "px");
+		$("#modulo").css("min-height", ($(document).height() - $(document).height() * 0.03) + "px");
 		//alert(screen.height + ' ' + screen.availHeight + ' ' + screen.pixelDepth + ' ' + $(document).height());
 	}
 	
